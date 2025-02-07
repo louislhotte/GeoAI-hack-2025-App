d5b35cc0f946c41e6ff11f6c516cfa1e509991c7
JasonMendoza2008 <lhotteromain@gmail.com>
2025-02-07 14:35:06 +0100

    Model page

--- Pages/2_Models.py
+++ Pages/2_Models.py
@@ -2,3 +2,11 @@ import streamlit as st
 
 st.title("ℹ️ Models")
 st.write("You can download models from here:")
+
+# List of the filenames qu'ils peuvent télécharger
+model_files = ["model_int8.ckpt", "model_fp16.ckpt", "instageo_best_checkpoint-v2.ckpt"]
+
+# Buttons
+for model in model_files:
+    with open(model, "rb") as file:
+        btn = st.download_button(label=f"Download {model}", data=file, file_name=model, mime="application/octet-stream")
